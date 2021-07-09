bf7c262d78d7487134232bff09548257f87270f9
viletyy <yystopf@163.com>
2021-07-09 14:26:02 +0800

    fix: change server init

--- config/server.go
+++ config/server.go
@@ -1,13 +1,14 @@
 /*
  * @Date: 2021-03-22 09:54:07
  * @LastEditors: viletyy
- * @LastEditTime: 2021-06-20 19:45:31
+ * @LastEditTime: 2021-07-09 14:22:47
  * @FilePath: /potato/config/server.go
  */
 package config
 
 type Server struct {
-	Port           int64  `mapstructure:"port" json:"port" yaml:"port"`
+	HttpPort       int64  `mapstructure:"http_port" json:"http_port" yaml:"http_port"`
+	GrpcPort       int64  `mapstructure:"grpc_port" json:"grpc_port" yaml:"grpc_port"`
 	ReadTimeout    int64  `mapstructure:"read_timeout" json:"read_timeout" yaml:"read_timeout"`
 	WriteTimeout   int64  `mapstructure:"write_timeout" json:"write_timeout" yaml:"write_timeout"`
 	TracerHostPort string `mapstructure:"tracer_host_port" json:"tracer_host_port" yaml:"tracer_host_port"`
--- initialize/grpc_server.go
+++ initialize/grpc_server.go
@@ -1,22 +1,28 @@
 /*
  * @Date: 2021-06-17 00:19:32
  * @LastEditors: viletyy
- * @LastEditTime: 2021-06-20 19:46:17
+ * @LastEditTime: 2021-07-09 14:24:14
  * @FilePath: /potato/initialize/grpc_server.go
  */
 package initialize
 
 import (
+	"net"
+
 	pb "github.com/viletyy/potato/proto/basic"
 	"github.com/viletyy/potato/server/basic"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
 
-func RunGrpcServer() *grpc.Server {
+func RunGrpcServer(port string) error {
 	server := grpc.NewServer()
 	pb.RegisterVendorServiceServer(server, basic.NewVendorServer())
 	reflection.Register(server)
 
-	return server
+	lis, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		return err
+	}
+	return server.Serve(lis)
 }
