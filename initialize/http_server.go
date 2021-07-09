3109e7db3debdcf5ac94e96c002ad0d30d2de32e
viletyy <yystopf@163.com>
2021-07-09 14:26:02 +0800

    fix: change server init

--- initialize/http_server.go
+++ initialize/http_server.go
@@ -1,12 +1,13 @@
 /*
  * @Date: 2021-03-22 17:03:27
  * @LastEditors: viletyy
- * @LastEditTime: 2021-06-20 22:22:10
+ * @LastEditTime: 2021-07-09 14:24:36
  * @FilePath: /potato/initialize/http_server.go
  */
 package initialize
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 
@@ -14,15 +15,15 @@ import (
 	"github.com/viletyy/potato/internal/routers"
 )
 
-func RunHttpServer(port string) *http.Server {
+func RunHttpServer(port string) error {
 	router := routers.InitRouter()
 	server := &http.Server{
-		Addr:           port,
+		Addr:           fmt.Sprintf(":%s", port),
 		Handler:        router,
 		ReadTimeout:    time.Duration(global.GO_CONFIG.Server.ReadTimeout) * time.Second,
 		WriteTimeout:   time.Duration(global.GO_CONFIG.Server.ReadTimeout) * time.Second,
 		MaxHeaderBytes: 1 << 20,
 	}
 
-	return server
+	return server.ListenAndServe()
 }
